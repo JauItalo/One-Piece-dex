aba7c5741188e887812ba4a110d81d793aab4ce6
Jau Italo <jau.batista@dcx.ufpb.br>
2026-02-12 20:12:54 -0300

    vamos adicionar o back

---
+++ onepiece-web/backend/onepiece-api/src/main/java/com/onepiece/onepiece_api/OnepieceApiApplication.java
@@ -0,0 +1,13 @@
+package com.onepiece.onepiece_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepieceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepieceApiApplication.class, args);
+	}
+
+}
