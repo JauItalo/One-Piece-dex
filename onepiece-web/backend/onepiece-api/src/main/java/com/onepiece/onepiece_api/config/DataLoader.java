cc04f5def23990ae089ecae17ec7af94e1e2e80e
Jau Italo <jau.batista@dcx.ufpb.br>
2026-02-13 18:23:44 -0300

    dataLoader implementado

--- onepiece-web/backend/onepiece-api/src/main/java/com/onepiece/onepiece_api/config/DataLoader.java
+++ onepiece-web/backend/onepiece-api/src/main/java/com/onepiece/onepiece_api/config/DataLoader.java
@@ -1,5 +1,44 @@
 package com.onepiece.onepiece_api.config;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.onepiece.onepiece_api.model.Character;
+import com.onepiece.onepiece_api.repository.CharacterRepository;
+@Configuration
 public class DataLoader {
+
+
+    @Bean
+    CommandLineRunner initDataBase(CharacterRepository characterRepository){
+        return args -> {
+            if (characterRepository.count() > 0)
+                return;
+
+            Character luffy = new Character();
+            luffy.setNome("Monkey D. Luffy");
+            luffy.setApelido("Chapéu de Palha");
+            luffy.setBando("Chapéus de Palha");
+            luffy.setCargo("Capitão");
+            luffy.setRecompensa("3.000.000.000");
+            luffy.setFrutaDoDiabo("Gomu Gomu no Mi");
+            luffy.setImagemUrl("https://static.wikia.nocookie.net/onepiece/images/6/6f/Luffy_Anime_Infobox.png");
+
+            Character zoro = new Character();
+            zoro.setNome("Roronoa Zoro");
+            zoro.setApelido("Caçador de Piratas");
+            zoro.setBando("Chapéus de Palha");
+            zoro.setCargo("Espadachim");
+            zoro.setRecompensa("1.111.000.000");
+            zoro.setFrutaDoDiabo("Nenhuma");
+            zoro.setImagemUrl("https://static.wikia.nocookie.net/onepiece/images/8/84/Roronoa_Zoro_Anime_Infobox.png");
+
+            characterRepository.saveAll(List.of(luffy,zoro));
+
+        };
+    }
     
 }
